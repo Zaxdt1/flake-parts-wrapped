b9638074718b7467c671063cf9298d72d1351d89
Zaxdt1 <zaxdt31@gmail.com>
2026-09-17 21:01:23 +0200

    Create flake.nix

---
+++ template/flake.nix
@@ -0,0 +1,28 @@
+ {
+        description = "A flake-parts project";
+        inputs = {
+          nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+          flake-parts.url = "github:hercules-ci/flake-parts";
+        };
+        outputs = inputs@{ flake-parts, ... }:
+          flake-parts.lib.mkFlake { inherit inputs; } {
+            systems = [
+              "x86_64-linux"
+              "aarch64-linux"
+          flake-parts.url = "github:hercules-ci/flake-parts";                                                                       
+        };                                                                                                                          
+        outputs = inputs@{ flake-parts, ... }:                                                                                      
+          flake-parts.lib.mkFlake { inherit inputs; } {                                                                             
+            systems = [                                                                                                             
+              "x86_64-linux"                                                                                                        
+              "aarch64-linux"                                                                                                       
+            ];                                                                                                                      
+            perSystem = { pkgs, ... }: {                                                                                            
+              devShells.default = pkgs.mkShell {                                                                                    
+                packages = with pkgs; [                                                                                             
+                  git                                                                                                               
+                ];                                                                                                                  
+              };                                                                                                                    
+            };                                                                                                                      
+          };                                                                                                                        
+      }          
